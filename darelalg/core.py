43bfdfde9536c72195a139756d1e655de9962d6c
sanjay krishnan <sanjaykrishnan@sanjays-air.attlocal.net>
2019-07-17 20:16:30 -0500

    Added more core routines

--- darelalg/core.py
+++ darelalg/core.py
@@ -1,6 +1,12 @@
 def attr(x):
 	return lambda t: (t[0] == x)
 
+def suffix(x):
+	return lambda t: (x in t[0])
+
+def label(x, label):
+	return lambda t: (label, t)
+
 def is_number(x):
 	try:
 		float(x)
@@ -16,12 +22,17 @@ def false(x):
 
 def match(a1, a2):
 
+	return jaccard(a1,a2, 1.0)
+
+
+def jaccard(a1, a2, thresh):
+
 	def _inner(tup):
 		left = set([a[1] for a in tup if a1(a)])
 		right = set([a[1] for a in tup if a2(a)])
+		jac = len(left.intersection(right))/len(left.union(right))
 
-		return left == right
+		return (jac >= thresh)
 
 	return _inner 
-
 
