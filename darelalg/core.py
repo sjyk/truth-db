8490ca49c51a0a722a9e2dc04b7b2e5e897138bb
sanjay krishnan <sanjaykrishnan@sanjays-MacBook-Air.local>
2019-07-23 16:49:35 -0500

    Updated with some new comments

--- darelalg/core.py
+++ darelalg/core.py
@@ -1,11 +1,25 @@
 def attr(x):
-	return lambda t: (t[0] == x)
+	def _inner(t):
+		try:
+			return t[0] == x
+		except:
+			return false
+
+	return _inner
+
 
 def suffix(x):
 	return lambda t: (x in t[0])
 
-def label(x, label):
-	return lambda t: (label, t)
+
+def label_if(label, cond):
+	def _inner(t):
+		if cond(t):
+			return (label, t)
+		else:
+			return t
+	return _inner
+
 
 def is_number(x):
 	try:
@@ -14,6 +28,15 @@ def is_number(x):
 	except:
 		return False
 
+
+def number_attributes(x):
+	try:
+		float(x[1])
+		return True
+	except:
+		return False
+
+
 def true(x):
 	return True
 
@@ -21,18 +44,33 @@ def false(x):
 	return False
 
 def match(a1, a2):
-
 	return jaccard(a1,a2, 1.0)
 
+def exists(x):
+	return lambda t: x in t
+
+
+def conj(a,b):
+	return lambda t: a(t) and b(t)
+
+def disj(a,b):
+	return lambda t: a(t) or b(t)
 
 def jaccard(a1, a2, thresh):
-
 	def _inner(tup):
 		left = set([a[1] for a in tup if a1(a)])
 		right = set([a[1] for a in tup if a2(a)])
 		jac = len(left.intersection(right))/len(left.union(right))
-
+		
 		return (jac >= thresh)
 
 	return _inner 
 
+def atl_one(a1, a2):
+	def _inner(tup):
+		left = set([a[1] for a in tup if a1(a)])
+		right = set([a[1] for a in tup if a2(a)])
+
+		return (len(left.intersection(right)) > 0)
+
+	return _inner
--- darelalg/rel.py
+++ darelalg/rel.py
@@ -1,35 +1,215 @@
+# -*- coding: utf-8 -*-
+"""This file is part of TruthDB which is released under MIT License and 
+is copyrighted by the University of Chicago Database Group.
+
+This module implements the intermediate relational representation for 
+TruthDB. Our objective is to unify query processing over structured and 
+unstructured data. To do so, we define a `dynamically-attributed` tuple.
+
+Every Dynamically-Attributed tuple is a set of atomic data, e.g.,
+{‘a’, ‘7’, ‘q’, ‘2019’}
+
+This set constitutes one tuple. A Dynamically-Attributed relation is a 
+multiset of such DA tuples:
+{	
+	{‘a’, ‘7’, ‘q’, ‘2019’},
+	{‘a’, ‘7’, ‘q’, ‘2019’, 4}
+}
+
+We can define select, project, join, and aggregate operations over these
+tuples akin to the definitions for standard relational algebra. 
+
+A tuple expression (tex) is Boolean function that maps from tuples to true, false. 
+Example: lambda tup: 'a' in tup
+
+An attribute expression (aex) is a boolean condition that maps dom->{true, false}. 
+Example: isNumber(value)
+
+So, then the basic algebra is:
+select(tex) #find all tuples that satisfy tex
+project(aex) #filter each tuple by only those values that satisfy aex
+groupby(aex) #group each tuple by those distinct values that satisfy aex
+union()
+difference()
+cart() 
+join(tex) 
+"""
+
 class DARelation(object):
+	"""DARelation: This class defines the main dynamically-attributed relation.
+
+	All operations on DARelation objects operate functionally making copies. Each
+	tuple is a python type(set). 
+
+	Tuple Expressions are functions that take in a set and output a boolean
+	Attribute Expressions are functions that take in a value and output a boolean
+	"""
+
 
 	def __init__(self, data):
+		"""DARelation constructor takes input data in the form of a collection
+		   of tuples.
+
+	    Args:
+	        data (Iterable[Iterable]): The input data.
+	    """
+
 		self.data = [set(rec) for rec in data]
 
-	def __or__(self, cond):
-		return DARelation([rec for rec in self.data if cond(rec)])
 
-	def __getitem__(self, key):
+	def __or__(self, tex):
+		"""select operator
 
-		try:
-		    iter(key)
-		    helper = lambda a: any([k(a) for k in key])
-		except TypeError as te:
-		    helper = lambda a: key(a)
+		select works similar to the relational algebra select operator. It
+		takes a set of tuples as input and returns a set of tuples that 
+		satisfy tex. We overload the | operator for syntactic reasons.
+
+		Args:
+	        tex (lambda tuple: boolean, tuple expression): A tuple expression
+
+	    Example:
+	    	(rel | lambda tup: 'a' in tup) 
+	    	# returns all tuples that have an 'a' value.
+	    """
+		return DARelation([rec \
+						  for rec in self.data 
+						  if tex(rec)])
+
+	def __getitem__(self, aex):
+		"""project operator
+
+		project is a generalization of the project operator seen in standard
+		relational algebra. Given a set of attribute expressions project 
+		restricts the set of attributes to those that satisfy the attribute
+		expression.
+
+		Args:
+	        aex (lambda value: boolean, value expression): A value expression
+
+	    Example:
+	    	rel[is_number]
+	    	#projects all tuples onto those attributes that are numerical
+	    """
+
+		if is_iterable(aex)
+		    helper = lambda a: any([k(a) for k in aex])
+		else:
+		    helper = lambda a: aex(a)
 		
-		return DARelation([set([attr for attr in rec if helper(attr)]) for rec in self.data])
 
 
-	def map(self, fn):
-		return DARelation([set([fn(attr) for attr in rec]) for rec in self.data])
+		marker = None
+		try:
+			data = []
+			for rec in self.data:
+				marker = rec
+				data.append(set([attr for attr in rec if helper(attr)]))
+			return DARelation(data)
+		except:
+			raise TupleTypeException("Error occurred at: " + str(marker))
+
 
 
 	def __mul__(self, other):
-		return DARelation([i.union(j) for i in other.data for j in self.data])
+		"""cartesian operator
 
-	def __add__(self, other):
-		return DARelation(self.data + other.data)
+		cartesian operator takes all pairs of tuples from two DArelations
+		and returns a new DARelation with the allpairs union.
+
+		Args:
+	        aex (lambda value: boolean, value expression): A value expression
+
+	    Example:
+	    	rel1*rel2
+
+		"""
+		return DARelation([i.union(j) for i in other.data for j in self.data])
+	
+
+	def group_by(self, key):
+		"""group by operator
+
+		The group by operator generates a new grouped relation. It groups
+		elements by the distinct values of the attribute expression. This 
+		function is implemented as a hash aggregation.
+
+		Args:
+	        key (lambda value: boolean, value expression): A value expression
+
+	    Example:
+	    	rel1.group_by(is_number)
+
+		"""
+
+		state = {}
+		for tup in self.data:
+			relevant_attrs = tuple([rec for rec in tup if key(rec)])
+
+			if len(relevant_attrs) == 0:
+				pass
+			
+			if relevant_attrs not in state:
+				state[relevant_attrs] = []
+
+			state[relevant_attrs].extend(tup)
+
+		return DARelation([list(k) + list(state[k]) for k in state])
+
+
+
+
+	def map(self, fn):
+		"""map operator
+
+		Applies a function to each value in the tuple
+
+		Args:
+	        fn (lambda value: value): A mapping function
+
+	    Example:
+	    	rel1.map(str)
+
+		"""
+		return DARelation([set([fn(attr) for attr in rec]) for rec in self.data])
+
+	def enumerate(self):
+		"""enumerate operator
+
+		Adds an id number to each tuple
+
+	    Example:
+	    	rel1.enumerate()
+
+		"""
+		return DARelation([rec.union(set([('id', i)])) for i,rec in enumerate(self.data)])
 
 	def __str__(self):
 		return str(self.data)
 
 	__repr__ = __str__
+
+
+
+def is_iterable(dat):
+	"""is_iterable tests to see if an object is iterable
+
+		Args:
+	        daata (object): an object
+	"""
+
+	try:
+		iter(data)
+		return True
+	except TypeError as te:
+		return False
+
+
+
+class TupleTypeException(Exception):
+	"""This is an uninteresting placeholder that defines a 
+	custom exception for now
+	"""
+	pass
+
 
 
